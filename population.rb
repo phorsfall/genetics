ce4e213d6bdcc01ac9acbecad2c6713baa0f06de
Paul Horsfall <horsfallp@gmail.com>
2010-07-17 13:44:39 +0100

    Added Population.

---
+++ population.rb
@@ -0,0 +1,34 @@
+class Population
+  def initialize(klass)
+    @klass = klass
+  end
+
+  def evolve
+    population_size = 500
+    population = Array.new(population_size) { @klass.generate }
+    population_with_fitness = []
+
+    500.times do
+      population_with_fitness = population.map { |t| [t.fitness, t] }
+      # TODO: Rename to ranked_population.
+      population_with_fitness.sort! { |a,b| a[0] <=> b[0] }
+      break if population_with_fitness[0][0] == 0
+      next_generation = []
+      next_generation << population_with_fitness[0][1] << population_with_fitness[1][1]
+      while next_generation.size < population_size
+        if rand > 0.05
+          next_generation << population_with_fitness[weighted_rand][1].mutate
+        else
+          next_generation << klass.generate
+        end
+      end
+      population = next_generation
+    end
+
+    population_with_fitness[0][1]
+  end
+
+  def weighted_rand(pexp = 0.8)
+    (Math.log10(rand) / Math.log10(pexp)).to_i
+  end
+end
---
+++ population_test.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'test/unit'
+require 'redgreen'
+require 'tree'
+require 'population'
+
+class PopulationTest < Test::Unit::TestCase
+  class SquareTree < Tree
+    arg :x
+
+    DATA = { 1 => 1, 2 => 4, 3 => 9, 4 => 16, 5 => 25, 6 => 36 }
+
+    def fitness
+      d = 0
+      self.class::DATA.each do |x, expected|
+        d += (expected - evaluate(:x => x)).abs
+      end
+      d
+    end
+  end
+
+  def test_evolving_a_population
+    population = Population.new(SquareTree)
+    tree = population.evolve
+    puts tree.inspect
+    assert_equal 1, tree.evaluate(:x => 1)
+    assert_equal 4, tree.evaluate(:x => 2)
+    assert_equal 9, tree.evaluate(:x => 3)
+    assert_equal 16, tree.evaluate(:x => 4)
+  end
+end
