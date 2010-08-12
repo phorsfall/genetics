db5eaea737412e759320cebfcfdacb5448cf8aec
Paul Horsfall <horsfallp@gmail.com>
2010-08-12 10:54:20 +0100

    Indentation.

--- lib/genetics/selection/segaran.rb
+++ lib/genetics/selection/segaran.rb
@@ -31,26 +31,26 @@ module SegaranVersusTournament
   def prepare
     scores = Array.new(size, 0)
 
-     # Is there a built-in (or Facets) function for getting each combination?
-     # Array#combination?
-     each_with_index do |tree1, tree1_index|
-       each_with_index do |tree2, tree2_index|
-         next if tree1.equal?(tree2)
+    # Is there a built-in (or Facets) function for getting each combination?
+    # Array#combination?
+    each_with_index do |tree1, tree1_index|
+      each_with_index do |tree2, tree2_index|
+        next if tree1.equal?(tree2)
 
-         # Only play against each other once.
-         # This risks evolving a program that is only successful playing as
-         # either player 1 OR player 2.
-         #next if tree1_index > tree2_index
+        # Only play against each other once.
+        # This risks evolving a program that is only successful playing as
+        # either player 1 OR player 2.
+        #next if tree1_index > tree2_index
 
-         result = tree1.vs(tree2)
-         scores[tree1_index] += result[0]
-         scores[tree2_index] += result[1]
-       end
-     end
+        result = tree1.vs(tree2)
+        scores[tree1_index] += result[0]
+        scores[tree2_index] += result[1]
+      end
+    end
 
-     population_with_scores = zip(scores)
-     population_with_scores.sort! { |a,b| b.last <=> a.last }
-     @ranked_population = population_with_scores.collect { |tree, score| tree }
+    population_with_scores = zip(scores)
+    population_with_scores.sort! { |a,b| b.last <=> a.last }
+    @ranked_population = population_with_scores.collect { |tree, score| tree }
   end
 
   def fittest(n = 1)
